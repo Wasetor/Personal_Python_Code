290dcb6c89be617a11862e2672c7c79b0c61deda
Wasetor <torsolbaba3@gmail.com>
2023-05-07 08:21:08 +0100

    my first string formatting lesson

---
+++ stringformatting.py
@@ -0,0 +1,8 @@
+name = "Solomon Tor Igbawase"
+fname = input("Insert Firstname: ")
+sname = input("Insert Surname: ")
+age = int(input("Insert age: "))
+
+# using the old style formatting
+print("Your name is %s %s and you are %d years old" %(fname, sname, age))
+print(" have an amazing day %s" %(name))
