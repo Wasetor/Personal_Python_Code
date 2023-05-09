7b4564365054bccad729e806b4b34da1f82bbff6
Wasetor <torsolbaba3@gmail.com>
2023-05-09 13:12:19 +0100

    still on loops and conditional statements alongside logical operators

---
+++ password_check.py
@@ -0,0 +1,15 @@
+username = "admin"
+password = "@H311o2ua1!"
+
+# want to write some block of codes that will check if username and password are correct
+user_username: str = input("Enter Username ").lower()
+user_password = input("Enter Password ")
+# I will have to use condition statements to check the correcteness
+if user_password == password and user_username == username:
+    print("log in successful")
+elif user_password != password and user_username != username:
+    print("Incorrect username and password")
+elif user_username != username:
+    print("Incorrect Username")
+else:
+    print("Incorrect password")
---
+++ while_loop.py
@@ -0,0 +1,6 @@
+counter = 1 # initialization of the while loop
+while (counter <= 10): # condition for which the loop will run
+        counter = counter + 1  # increment or decrement
+        print (counter)  # action that will be carried out if the loop is true
+else:
+    print("Your loop finally finished executing") # action if the loop is false
